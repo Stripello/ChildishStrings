ba4437522b59aed966aa1fb7b86eaacd01aa99c0
Владлен Шапарев <shve@outlook.com>
2022-04-22 17:26:40 +0700

    Add project files.

---
+++ CapitalsAndStates.cs
@@ -0,0 +1,19 @@
+﻿namespace Informatic
+{
+    internal class CapitalsAndStates
+    {
+        internal static void WriteSomeStrings()
+        {
+            Console.WriteLine("Enter state.");
+            var state = Console.ReadLine();
+            Console.WriteLine("Enter capital of this state.");
+            var capital = Console.ReadLine();
+
+            Console.WriteLine($"By string interpolation: Столица государства {state} - город {capital}.");
+
+            var answer = String.Format("Столица государства {0} - город {1}.",state,capital);
+            Console.WriteLine("By String.Format: " + answer);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Informatic;
+
+var inf = "информатика";
+Console.WriteLine(inf.Substring(2, 5));
+Console.WriteLine(inf.Substring(7, 3));
+
+Console.WriteLine();
+var virus = "вирус";
+Console.WriteLine(virus.Replace('в', 'ф').Replace('и', 'о').Replace('р', 'к'));
+
+Console.WriteLine();
+var wrong = "рпоцессор";
+var wrongChar = wrong.ToCharArray();
+(wrongChar[0], wrongChar[1]) = (wrongChar[1], wrong[0]);
+wrong =new  String(wrongChar);
+Console.WriteLine(wrong);
+
+Console.WriteLine();
+CapitalsAndStates.WriteSomeStrings();
+
